b1fb1c836aa1941011e2e8f2cb03ffb4c97c8650
c0c0pang <coc1789@naver.com>
2023-05-04 00:46:50 +0900

    feat: 회원가입시 토큰,유혀성 검사를 실시하는 로직 제작 및 페이지 제작

--- src/signin_signup/Signup.jsx
+++ src/signin_signup/Signup.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { MainDiv, SelectBoxDiv } from '../styled_component/styled_main'
+import { Input, Button } from '../styled_component/styled_signin'
+import { authApi } from '../api/apiStorage'
+import { useNavigate } from 'react-router-dom'
+import { Validation, TokenCheck } from '../custom/functionSet'
 function Signup() {
+    const [inputs, setInputs] = useState({ email: '', password: '' })
+    const { email, password } = inputs
+    const [check, setCheck] = useState(true)
+    const navigate = useNavigate()
+    useEffect(() => {
+        //토큰이 검사
+        TokenCheck({ navigate })
+    }, [])
+    const onChange = (e) => {
+        const { name, value } = e.target
+        setInputs({ ...inputs, [name]: value })
+        setCheck(true)
+    }
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        const msg = await authApi.SignupAuth(inputs)
+        //msg가 true이면 정상 응답이므로 signin으로 리다이렉트
+        if (msg === true) {
+            navigate('/signin')
+        }
+    }
+    const onClick = () => {
+        //유효성 검사
+        Validation({ email, password, setCheck })
+    }
     return (
-        <div>signup</div>
+        <MainDiv>
+            <SelectBoxDiv onSubmit={onSubmit}>
+                <Input type='email' name='email' value={email} placeholder='이메일 입력' onChange={onChange} data-testid="email-input"></Input>
+                <Input type='password' name='password' value={password} onChange={onChange} placeholder='비밀번호 입력' minLength={8} data-testid="password-input"></Input>
+                {check ? (<Button type='submit' data-testid="signin-button" onClick={onClick}>회원가입</Button>) : (<Button disabled>회원가입</Button>)}
+            </SelectBoxDiv>
+        </MainDiv>
     )
 }
 
