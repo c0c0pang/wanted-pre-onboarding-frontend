92cf72e2f028a83c84d2676e9ca743f1880d6993
c0c0pang <coc1789@naver.com>
2023-05-04 00:46:18 +0900

    feat: 로그인에서 유효성,토큰 검사를 하는 로직 제작

--- src/signin_signup/Signin.jsx
+++ src/signin_signup/Signin.jsx
@@ -1,27 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MainDiv, SelectBoxDiv } from '../styled_component/styled_main'
 import { Input, Button } from '../styled_component/styled_signin'
+import { authApi } from '../api/apiStorage'
+import { useNavigate } from 'react-router-dom'
+import { TokenCheck, Validation } from '../custom/functionSet'
 function Signin() {
     const [inputs, setInputs] = useState({ email: '', password: '' })
     const { email, password } = inputs
     const [check, setCheck] = useState(true)
+    const navigate = useNavigate()
+    useEffect(() => {
+        //토큰 검사
+        TokenCheck({ navigate })
+    }, [])
     const onChange = (e) => {
         const { name, value } = e.target
         setInputs({ ...inputs, [name]: value })
         setCheck(true)
     }
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
+        const msg = await authApi.SigninAuth(inputs)
+        //msg가 정상응답이면 /todo로 리다이렉트
+        if (msg) {
+            window.localStorage.setItem('access_token', msg)
+            navigate('/todo')
+        }
     }
     const onClick = () => {
-        if (!email.includes('@')) {
-            setCheck(false)
-            return
-        }
-        if (password.length < 8) {
-            setCheck(false)
-            return
-        }
+        //유효성 검사
+        Validation({ email, password, setCheck })
     }
     return (
         <MainDiv>
@@ -29,7 +37,6 @@ function Signin() {
                 <Input type='email' name='email' value={email} placeholder='이메일' onChange={onChange} data-testid="email-input"></Input>
                 <Input type='password' name='password' value={password} onChange={onChange} placeholder='비밀번호' minLength={8} data-testid="password-input"></Input>
                 {check ? (<Button type='submit' data-testid="signin-button" onClick={onClick}>로그인</Button>) : (<Button disabled>로그인</Button>)}
-
             </SelectBoxDiv>
         </MainDiv>
     )
