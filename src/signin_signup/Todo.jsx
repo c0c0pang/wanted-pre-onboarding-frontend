4fb9f43ac3e9b1c8154e7352544e628eb2cd1be8
c0c0pang <coc1789@naver.com>
2023-05-20 22:05:15 +0900

    feat: 토근 검사 함수 교체 및 styled-component 반영

--- src/signin_signup/Todo.jsx
+++ src/signin_signup/Todo.jsx
@@ -1,15 +1,32 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { TokenCheck } from '../custom/functionSet'
-
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { TokenCheckTodo } from "../custom/functionSet";
+import {
+  TodoCreateDiv,
+  Label,
+  LabelDiv,
+  LabelInput,
+  LabelSpan,
+  List,
+  ListDiv,
+  ModifyCancelButton,
+  ModifyDiv,
+  ModifyInput,
+  ModifySubmitButton,
+  TodoCreateButton,
+  TodoCreateInput,
+} from "../styled_component/styled_todo";
 function Todo() {
-    const navigate = useNavigate()
-    useEffect(() => {
-        TokenCheck({ navigate })
-    }, [])
-    return (
-        <div>Todo</div>
-    )
+  const navigate = useNavigate();
+  useEffect(() => {
+    TokenCheckTodo({ navigate });
+  }, []);
+  return (
+    <TodoCreateDiv>
+      <TodoCreateInput />
+      <TodoCreateButton>추가</TodoCreateButton>
+    </TodoCreateDiv>
+  );
 }
 
-export default Todo
+export default Todo;
