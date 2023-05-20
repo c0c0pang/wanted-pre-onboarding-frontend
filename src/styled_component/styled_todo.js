27d1e2d6e44091bdfc6c2fe34322df7a2b5465f7
c0c0pang <coc1789@naver.com>
2023-05-20 22:03:19 +0900

    feat: todo styled-component 추가

---
+++ src/styled_component/styled_todo.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const TodoCreateDiv = styled.div`
+
+`
+
+export const TodoCreateInput = styled.input`
+
+`
+export const TodoCreateButton = styled.button`
+
+`
+export const ListDiv = styled.div`
+
+`
+
+export const List = styled.li`
+    
+`
+
+export const LabelDiv = styled.div`
+    
+`
+export const Label = styled.label`
+    
+`
+export const LabelInput = styled.input`
+
+`
+export const LabelSpan = styled.span`
+
+`
+export const ModifyDiv = styled.div`
+
+`
+export const ModifyInput = styled.div`
+
+`
+export const ModifySubmitButton = styled.div`
+
+`
+export const ModifyCancelButton = styled.div`
+
+`
