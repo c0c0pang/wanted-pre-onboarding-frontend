b3e1b338c39bf3b0ce25fa72eb056fd844298f73
c0c0pang <coc1789@naver.com>
2023-05-22 06:29:27 +0900

    feat: Todo의 기능중 생성,가져오기,삭제 api 추가

--- src/api/apiStorage.js
+++ src/api/apiStorage.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import { TOKEN } from "../custom/functionSet";
 
 const baseURL = axios.create({
-    baseURL: ' https://www.pre-onboarding-selection-task.shop'
+    baseURL: 'https://www.pre-onboarding-selection-task.shop'
 })
-
+const config = {
+    headers: {
+        'Authorization': 'Bearer ' + TOKEN
+    }
+}
 class Auth {
     SigninAuth = async (props) => {
         return await baseURL.post('/auth/signin', {
@@ -20,8 +25,24 @@ class Auth {
 }
 
 class Todo {
-    CreateTodo = async (props) => {
-
+    CreateTodo = async (todo) => {
+        return await baseURL.post('/todos',
+            {
+                todo: todo
+            },
+            config
+        ).then((res) => {
+            console.log(res.status)
+        })
+    }
+    GetTodo = async () => {
+        return await baseURL.get('/todos', config)
+            .then((res) => res.data)
+    }
+    DeleteTodo = async (id) => {
+        return await baseURL.delete(`/todos/${id}`, config)
+            .then((res) => res.status)
     }
 }
 export const authApi = new Auth()
+export const todoApi = new Todo()
