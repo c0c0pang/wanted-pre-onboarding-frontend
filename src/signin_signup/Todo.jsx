bc55bcaae99031b8e78120fb2cdab2c020706b90
c0c0pang <coc1789@naver.com>
2023-05-23 02:43:15 +0900

    feat: 수정 기능 추가

--- src/signin_signup/Todo.jsx
+++ src/signin_signup/Todo.jsx
@@ -23,7 +23,8 @@ function Todo() {
   const navigate = useNavigate();
   const [text, setText] = useState('')
   const [listBox, setListBox] = useState([])
-  const [checkList, setCheckList] = useState([])
+  const [input, setInput] = useState({})
+  const [check, setCheck] = useState({})
   useEffect(() => {
     TokenCheckTodo({ navigate });
     const listSetup = async () => {
@@ -44,32 +45,41 @@ function Todo() {
   }
 
   const listComponent = () => {
-    const modifyButton = (index) => {
-      const newList = [...listBox]
-      newList[index] = { ...listBox[index], 'isCompleted': true }
-      setListBox(newList)
+    const modifyButton = (index, todo, e) => {
+      setCheck({ ...check, [index]: true })
+      setInput({ ...input, [e.target.name]: todo })
     }
     const cancelButton = (index) => {
-      const newList = [...listBox]
-      newList[index] = { ...listBox[index], 'isCompleted': false }
-      setListBox(newList)
+      setCheck({ ...check, [index]: false })
+    }
+    const onChange = (e) => {
+      const { name, value } = e.target
+      setInput({ ...input, [name]: value })
+    }
+    const onSubmit = async (id, todo, index) => {
+      const isCompleted = true
+      await todoApi.UpDateTodo({ id, todo, isCompleted })
+      const updateList = await todoApi.GetTodo()
+      setListBox([...updateList])
+      setCheck({ ...check, [index]: false })
+
     }
     return (
       listBox.map((element, index) =>
         <List key={index}>
           <Label>
             <LabelInput type="checkbox"></LabelInput>
-            <LabelSpan>{element.todo}</LabelSpan>
           </Label>
-          {element.isCompleted ? (
+          {check?.[index] ? (
             <>
-              <ModifyInput type="text" data-testid="modify-input"></ModifyInput>
-              <ModifySubmitButton data-testid="submit-button">제출</ModifySubmitButton>
+              <ModifyInput onChange={(e) => onChange(e)} name={element.id.toString()} value={input?.[element.id]} type="text" data-testid="modify-input"></ModifyInput>
+              <ModifySubmitButton data-testid="submit-button" onClick={() => onSubmit(element.id, input?.[element.id], index)}>제출</ModifySubmitButton>
               <ModifyCancelButton data-testid="cancel-button" onClick={() => cancelButton(index)}>취소</ModifyCancelButton>
             </>
           ) : (
             <>
-              <ModifyButton data-testid="modify-button" onClick={() => modifyButton(index)}>수정</ModifyButton>
+              <LabelSpan>{element.todo}</LabelSpan>
+              <ModifyButton name={element.id.toString()} data-testid="modify-button" onClick={(e) => modifyButton(index, element.todo, e)}>수정</ModifyButton>
               <DeleteButton data-testid="delete-button" onClick={() => deleteTodo(element.id)}>삭제</DeleteButton>
             </>
           )}
